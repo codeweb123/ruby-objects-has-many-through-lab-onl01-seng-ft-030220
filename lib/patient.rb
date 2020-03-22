e5429960e254105646216eadf98e54c217521e8b
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 19:54:55 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,7 @@ class Patient
   end
 
   def doctors
-    Appointment.all.collect {|appointment| appointment.doctor}
+    Appointment.all.select {|appointment| appointment.doctor}
   end
 
   def new_appointment(date, doctor)
