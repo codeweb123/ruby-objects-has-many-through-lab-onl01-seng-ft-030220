50474b5058f57fc06d1eca9eee958c56c38374ca
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 21:29:51 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment == self}
+    Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def doctors
