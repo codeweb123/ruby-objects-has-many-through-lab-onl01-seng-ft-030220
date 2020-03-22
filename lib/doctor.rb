e08aef48c2f0bd86474a6f3d2cbb0b3c0042cfe3
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-22 21:06:56 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,31 +1,23 @@
 class Doctor
-  @@all=[]
-
-  attr_accessor :name, :appointments, :patient
+  attr_accessor :name, :add_appointment
 
   def initialize(name)
     @name = name
     @appointments = []
-    @@all << self
+  end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.doctor = self
   end
 
   def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
+    @appointments
   end
 
   def patients
     @appointments.collect do |appointment|
       appointment.patient
     end
-  end
-
-  def self.all
-    @@all
-  end
-
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-    @appointment << appointment
-    appointment
   end
 end
